998c260c9dfc8c3033c6598e9f2ee5f813fe355b
ahfazmalik <ahfaz.malik@olivedigital.com>
2023-09-19 05:25:23 +0500

    CompanyPostJobApi

---
+++ app/Http/Controllers/ApiControllers/companies/auth/LoginController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\ApiControllers\companies\auth;
+
+use App\Http\Controllers\Controller;
+use App\Services\CompanyService;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    protected $companyService;
+    public function __construct(CompanyService $companyService)
+    {
+        $this->companyService = $companyService;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => 'Validation failed',
+            'errors' => $validator->errors(),
+        ], 422));
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        try {
+            // Attempt to authenticate the company
+            $authResult = $this->companyService->authenticateCompany($credentials);
+            return response()->json($authResult);
+        }catch (\Exception $e){
+            return response()->json([
+                "success" => false,
+                "message" => $e->getMessage(),
+            ]);
+        }
+    }
+
+    public function logout()
+    {
+        try {
+            $response = $this->companyService->logoutCompany(Auth::guard('company_api'));
+            return response()->json($response);
+        }catch (\Exception $e){
+            return response()->json([
+                "success" => false,
+                "message" => $e,
+            ]);
+        }
+
+    }
+}
